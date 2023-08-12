6f61359d3ba8beb60b8619ab0c878b36e73cc985
hamzamandili02 <hamzamandili02@gmail.com>
2023-08-12 05:04:25 +0200

    1st version of food websit

---
+++ vuejs-search-meals/src/components/YoutubeButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <a
+            :href="href"
+            target="_blank"
+            class="px-3 py-2 rounded border-2 border-red-600 text-white bg-red-600 hover:bg-red-600 hover:transition-colors">
+            <slot>Youtube</slot>
+            </a>
+</template>
+<script setup>
+const props = defineProps({
+  href: String
+})
+</script>
---
+++ vuejs-search-meals/src/views/MealDetails.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="w-800 mx-auto p-8">
+    <pre>{{ meal }}</pre>
+    <h1 class="text-5xl font-bold mb-5">{{ meal.strMeal }}</h1>
+    <img :src="meal.strMealThumb" :alt="meal.strMeal" />
+    <div class="grid grid-cols-1 sm:grid-cols-3 text-lg py-2">
+      <div>
+        <strong class="font-bold">Category:</strong> {{ meal.strCategory }}
+      </div>
+      <div><strong class="font-bold">Area:</strong> {{ meal.strArea }}</div>
+      <div><strong class="font-bold">Tags:</strong> {{ meal.strTags }}</div>
+    </div>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2">
+      <div>
+        <h2 class="text-2xl font-semibold mb-2">Ingredients</h2>
+        <ul>
+          <template v-for="(el, ind) of new Array(20)" :key="ind">
+            <li v-if="meal[`strIngredient${ind + 1}`]">
+              {{ ind + 1 }}. {{ meal[`strIngredient${ind + 1}`] }}
+            </li>
+          </template>
+        </ul>
+      </div>
+      <div>
+        <h2 class="text-2xl font-semibold mb-2">Measures</h2>
+        <ul>
+            <template v-for="(el, ind) of new Array(20)" :key="ind">
+              <li v-if="meal[`strMeasure${ind + 1}`]">
+             {{ ind + 1 }}. {{ meal[`strMeasure${ind + 1}`] }}
+             </li>
+          </template>
+        </ul>
+      </div>
+      <div class="mt-4">
+        <YoutubeButton :href="meal.strYoutube"> Youtube</YoutubeButton>
+        <a
+            :href="href"
+            target="_blank"
+            class="px-3 py-2 rounded text-indigo-600  hover:bg-indigo-600 hover:text-white transition-colors">
+            View Original Source
+            </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+import axiosClient from "../axiosClient";
+import YoutubeButton from '../components/YoutubeButton.vue';
+
+const route = useRoute();
+const meal = ref({});
+const mealIngredients = ref([]);
+
+onMounted(async () => {
+  try {
+    const response = await axiosClient.get(`lookup.php?i=${route.params.id}`);
+    meal.value = response.data.meals[0] || {};
+    mealIngredients.value = getIngredientsArray(meal.value);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+});
+
+function getIngredientsArray(meal) {
+  const ingredients = [];
+  for (let i = 1; i <= 20; i++) {
+    const ingredient = meal[`strIngredient${i}`];
+    if (ingredient) {
+      ingredients.push(ingredient);
+    }
+  }
+  return ingredients;
+}
+</script>
